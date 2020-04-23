ebfbd1a43bde7da6202e5087cda776fdc1c254e1
Steven Craig <38326910+dcsteve24@users.noreply.github.com>
2020-04-23 17:38:42 -0600

    Add files via upload
    Simple script to refresh (like brand new) email profiles

---
+++ Delete_All_Email_Profiles.ps1
@@ -0,0 +1,13 @@
+﻿#Deletes Users Exchange Profile if it hasnt been done before (checks for file it creates when ran)
+
+$checkFile = "C:\Users\Public\OutlookRefreshed.txt"
+
+if (!(Test-Path $CheckFile)) { #if file not present
+    New-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\AutoDiscover -Name ZeroConfigExchange -Value 1 -PropertyType DWORD -Force
+
+    Remove-Item "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\*" -Recurse -Force
+
+    Remove-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Setup\ -name First-Run
+
+    New-Item $checkFile
+}
